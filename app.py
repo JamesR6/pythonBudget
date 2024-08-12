b4492336bcbb587b453759c1645f2e3df8309d71
James Reeves <jpascualsr06@gmail.com>
2024-08-11 21:29:47 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,101 @@
+from flask import Flask, render_template, request
+import sqlite3;
+app = Flask(__name__)
+
+
+totalSpent=0
+
+# --------------------------
+@app.route('/', methods=['GET', 'POST'])
+def hello():
+    con = sqlite3.connect("./july.db")
+    cur = con.cursor()
+    if request.method == 'POST':
+        day = request.form.get('day')
+        typ = request.form.get('type')
+        amt = request.form.get('amount')
+        notes = request.form.get('notes')
+        rpu = request.form.get('removePurchase')
+        rpa = request.form.get('removePayback')
+        rpt = request.form.get('removeTransfer')
+        d = request.form.get('debit')
+        rd = request.form.get('removeDebit')
+        if(d!=None):
+            cur.execute("INSERT INTO debit(amount) VALUES (?)", (d,))
+        if(rpu!=None):
+            cur.execute("DELETE FROM purchases WHERE id = ?", (rpu,))
+        if(rpa!=None):
+            cur.execute("DELETE FROM payback WHERE id = ?", (rpa,))
+        if(rpt!=None):
+            cur.execute("DELETE FROM transfer WHERE id = ?", (rpt,))
+        if(rd!=None):
+            cur.execute("DELETE FROM debit WHERE id = ?", (rd,))
+        if(typ=="spent"):
+            cur.execute("INSERT INTO purchases(day, amount, notes) VALUES (?,?,?)", [ day, amt, notes])
+        elif(typ=="gained"):
+            cur.execute("INSERT INTO payback(day, amount, notes) VALUES (?,?,?)", [ day, amt, notes])
+        elif(typ=="transfer"):
+            cur.execute("INSERT INTO transfer(day, amount) VALUES (?,?)", [ day, amt])
+        con.commit()
+
+    # maths
+    limit = 1000
+
+# PURCHASES AND PAYBACK
+    cur.execute("SELECT SUM(amount) FROM purchases")
+    try:
+        totalSpent=round(cur.fetchone()[0], 2)
+    except:
+        totalSpent = 0
+    cur.execute("SELECT SUM(amount) FROM payback")
+    totalLeft = round(limit - totalSpent, 2)
+    try:
+        totalGained= round(cur.fetchone()[0], 2)
+    except:
+        totalGained = 0
+
+# AVERAGES
+    cur.execute("SELECT COUNT(*) FROM purchases")
+    days = round(cur.fetchone()[0])
+    if(days==0): days=1
+    average = round(totalSpent/days, 2)
+    fAverage = round(totalLeft/(31-days), 2)
+    leftWith = limit - round(average*31)
+
+# TRANSFERRED
+    cur.execute("SELECT SUM(amount) FROM transfer")
+    try:
+        amtTrans = round(cur.fetchone()[0], 2)
+    except:
+        amtTrans = 0
+    hts = round(totalSpent + amtTrans, 2)
+    htl = round(limit-hts, 2)
+ 
+# DEBIT
+    cur.execute("SELECT SUM(amount) FROM debit")
+    debit=round(cur.fetchone()[0], 2)
+
+    # end maths
+
+    cur.execute("SELECT * FROM payback")
+    payback = cur.fetchall()
+
+    cur.execute("SELECT * FROM purchases")
+    purchases = cur.fetchall()
+
+    cur.execute("SELECT * FROM transfer")
+    transfer = cur.fetchall()
+
+    cur.execute("SELECT * FROM debit")
+    debitList = cur.fetchall()[::-1]
+
+    con.close()
+    return render_template('index.html', purchases=purchases, payback=payback, totalSpent=totalSpent, totalLeft=totalLeft, totalGained=totalGained, average=average, fAverage=fAverage, leftWith=leftWith, debit=debit, debitList=debitList, transfer=transfer, hts=hts, amtTrans=amtTrans, htl=htl)
+
+
+
+
+# -------------------------
+
+if __name__ == '__main__':
+    app.run(debug=True)

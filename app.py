fd70a7bc9e682288a13d207323d71e4f861a3d6f
JamesR6 <jpascualsr06@gmail.com>
2024-12-04 16:46:18 -0600

    sdf

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ totalSpent=0
 # --------------------------
 @app.route('/', methods=['GET', 'POST'])
 def hello():
-    filename = "october.db"
+    filename = "november.db"
     con = sqlite3.connect(filename)
     cur = con.cursor()
     if request.method == 'POST':

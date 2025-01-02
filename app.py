7d3137a6aa0125e313f7ebc269c5d846b4cde732
JamesR6 <jpascualsr06@gmail.com>
2025-01-01 21:12:09 -0600

    purple

--- app.py
+++ app.py
@@ -3,106 +3,128 @@ import sqlite3;
 app = Flask(__name__)
 
 
-totalSpent=0
-
-# --------------------------
 @app.route('/', methods=['GET', 'POST'])
 def hello():
+
+    # Initiate database
     filename = "december.db"
     con = sqlite3.connect(filename)
     cur = con.cursor()
+
     if request.method == 'POST':
+        # Gather form info
         day = request.form.get('day')
-        typ = request.form.get('type')
-        amt = request.form.get('amount')
+        transactionType = request.form.get('type')
+        amount = request.form.get('amount')
         notes = request.form.get('notes')
-        rpu = request.form.get('removePurchase')
-        rpa = request.form.get('removePayback')
-        rpt = request.form.get('removeTransfer')
-        d = request.form.get('debit')
-        rd = request.form.get('removeDebit')
-        if(d!=None):
-            cur.execute("INSERT INTO debit(amount) VALUES (?)", (d,))
-        if(rpu!=None):
-            cur.execute("DELETE FROM purchases WHERE id = ?", (rpu,))
-        if(rpa!=None):
-            cur.execute("DELETE FROM payback WHERE id = ?", (rpa,))
-        if(rpt!=None):
-            cur.execute("DELETE FROM transfer WHERE id = ?", (rpt,))
-        if(rd!=None):
-            cur.execute("DELETE FROM debit WHERE id = ?", (rd,))
-        if(typ=="spent"):
-            cur.execute("INSERT INTO purchases(day, amount, notes) VALUES (?,?,?)", [ day, amt, notes])
-        elif(typ=="gained"):
-            cur.execute("INSERT INTO payback(day, amount, notes) VALUES (?,?,?)", [ day, amt, notes])
-        elif(typ=="transfer"):
-            cur.execute("INSERT INTO transfer(day, amount) VALUES (?,?)", [ day, amt])
+        debit = request.form.get('debit')
+
+        # Gather data to remove elements
+        removePurchase = request.form.get('removePurchase')
+        removeTransfer = request.form.get('removeTransfer')
+        removeDebit = request.form.get('removeDebit')
+
+        # Remove elements if requested
+        if(removePurchase != None):
+            cur.execute("DELETE FROM purchases WHERE id = ?", (removePurchase,))
+        if(removeTransfer != None):
+            cur.execute("DELETE FROM transfer WHERE id = ?", (removeTransfer,))
+        if(removeDebit != None):
+            cur.execute("DELETE FROM debit WHERE id = ?", (removeDebit,))
+
+        # Add elements if requested
+        if(debit != None):
+            cur.execute("INSERT INTO debit(amount) VALUES (?)", (debit,))
+        elif(transactionType == "spent"):
+            cur.execute("INSERT INTO purchases(day, amount, notes) VALUES (?,?,?)", [ day, amount, notes])
+        elif(transactionType == "gained"):
+            cur.execute("INSERT INTO payback(day, amount, notes) VALUES (?,?,?)", [ day, amount, notes]) 
+        elif(transactionType == "transfer"):
+            cur.execute("INSERT INTO transfer(day, amount) VALUES (?,?)", [ day, amount])
+
+        # Commit to database
         con.commit()
 
-    # maths
+
+
+# Monthly limit
     limit = 1000
 
-# PURCHASES AND PAYBACK
+# PURCHASES
+    # Gather sum spent
     cur.execute("SELECT SUM(amount) FROM purchases")
     try:
         totalSpent=round(cur.fetchone()[0], 2)
     except:
         totalSpent = 0
-    cur.execute("SELECT SUM(amount) FROM payback")
-    try:
-        totalGained= round(cur.fetchone()[0], 2)
-    except:
-        totalGained = 0
+
+    # Math sum spent
     forHard = totalSpent
-    totalSpent -= totalGained
     totalLeft = round(limit - totalSpent, 2)
 
 # AVERAGES
+    # Gather days entered
     cur.execute("SELECT COUNT(*) FROM purchases")
     days = round(cur.fetchone()[0])
     if(days==0): days=1
     if(days>30): days=30
-    average = round(totalSpent/days, 2)
-    fAverage = round(totalLeft/(31-days), 2)
-    leftWith = limit - round(average*31)
+
+    # Math averages
+    dailyAverage = round(totalSpent/days, 2)
+    futureAverage = round(totalLeft/(31-days), 2)
+    leftWith = limit - round(dailyAverage*31)
 
 # TRANSFERRED
+    # Gather sum transferred
     cur.execute("SELECT SUM(amount) FROM transfer")
     try:
-        amtTrans = round(cur.fetchone()[0], 2)
+        totalTransferred = round(cur.fetchone()[0], 2)
     except:
-        amtTrans = 0
-    hts = round(forHard + amtTrans, 2)
-    htl = round(limit-hts, 2)
+        totalTransferred = 0
+
+    # Math transferred
+    hardTotalSpent = round(forHard + totalTransferred, 2)
+    hardTotalLeft = round(limit - hardTotalSpent, 2)
  
 # DEBIT
     cur.execute("SELECT SUM(amount) FROM debit")
     try:
-        debit = cur.fetchone()[0]
+        totalDebit = cur.fetchone()[0]
     except:
-        debit = 0
+        totalDebit = 0
 
-    # end maths
 
-    cur.execute("SELECT * FROM payback")
-    payback = cur.fetchall()
-
+# Gather actual table rows from database
     cur.execute("SELECT * FROM purchases")
-    purchases = cur.fetchall()
+    purchaseRows = cur.fetchall()
 
     cur.execute("SELECT * FROM transfer")
-    transfer = cur.fetchall()
+    transferRows = cur.fetchall()
 
     cur.execute("SELECT * FROM debit")
-    debitList = cur.fetchall()[::-1]
+    debitRows = cur.fetchall()[::-1]
 
+# Close database
     con.close()
-    return render_template('index.html', purchases=purchases, payback=payback, totalSpent=totalSpent, totalLeft=totalLeft, totalGained=totalGained, average=average, fAverage=fAverage, leftWith=leftWith, debit=debit, debitList=debitList, transfer=transfer, hts=hts, amtTrans=amtTrans, htl=htl, filename = filename)
+
+# Return variables
+    return render_template('index.html', 
+    filename = filename,
+    totalSpent = totalSpent, 
+    amtTrans = totalTransferred, 
+    debit = totalDebit, 
+    totalLeft = totalLeft, 
+    hts = hardTotalSpent, 
+    htl = hardTotalLeft, 
+    average = dailyAverage, 
+    fAverage = futureAverage, 
+    leftWith=  leftWith, 
+    purchases = purchaseRows, 
+    transfer = transferRows, 
+    debitList = debitRows)
 
 
 
-
-# -------------------------
 
 if __name__ == '__main__':
     app.run(debug=True)

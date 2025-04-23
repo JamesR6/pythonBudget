10e16853fc1446934128a7b7fe09c3dbf4aacc01
JamesR6 <jpascualsr06@gmail.com>
2025-04-23 18:16:40 -0500

    aidbg

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello():
 
     # Initiate database
-    path = "./months/march.db"
+    path = "./months/april.db"
     con = sqlite3.connect(path)
     cur = con.cursor()
 
